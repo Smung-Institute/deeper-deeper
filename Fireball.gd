d08851ff0b400151344c8f7d1767fccf6322eca0
Ben <ishmandoo@gmail.com>
2021-04-24 18:09:23 -0400

    got fireballs working

--- Fireball.gd
+++ Fireball.gd
@@ -1,13 +1,16 @@
 extends Area2D
 
 var SPEED = 150
-var VELOCITY_VECTOR = SPEED * Vector2(1,0).rotated(PI/6)
+var velocity_vector = SPEED * Vector2(1,0).rotated(PI/6)
 
 func _ready():
+	print("fireball")
 	connect("body_entered", self, "_handle_body_entered")
+	velocity_vector.x = velocity_vector.x * scale.x
 	
 func _handle_body_entered(body):
 	print("diedie")
 	
 func _process(delta):
-	position += VELOCITY_VECTOR * delta
+	
+	position += velocity_vector * delta
--- Statue.gd
+++ Statue.gd
@@ -1,13 +1,26 @@
 extends StaticBody2D
 
+var FIREBALL_FREQUENCY = 2
+
 var fireball_scene = preload("res://Fireball.tscn")
 
 var time_start = 0
-var time_now = 0
+var ready_to_shoot = true
 
 func _ready():
 	time_start = OS.get_unix_time()
-	var fireball = fireball_scene.instance()
 
-	add_child(fireball)	
+func _process(delta):
+	if (OS.get_unix_time() - time_start)%FIREBALL_FREQUENCY == 1:
+		if ready_to_shoot:
+			shoot()
+			ready_to_shoot = false
+	else:
+		ready_to_shoot = true
 	
+	
+func shoot():
+	var fireball = fireball_scene.instance()
+	fireball.position = position
+	fireball.scale = scale
+	get_parent().add_child(fireball)	
