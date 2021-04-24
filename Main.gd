4b288b37472e5c8c7f3b0f1b15370e1bf524c37d
ducksoftheworld <ducksoftheworld@users.noreply.github.com>
2021-04-24 00:28:28 -0400

    david end of nite

---
+++ Main.gd
@@ -0,0 +1,36 @@
+extends Node
+
+var screensize = Vector2(600,400)
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$WalkButton.connect("toggled", self, "_on_walkbutton_toggled")
+	$ReturnButton.connect("pressed", self, "on_returnbutton_pressed")
+	$Person1.is_controlled = false
+	$Person2.is_controlled = true
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		$Person1.is_controlled = !$Person1.is_controlled
+		$Person2.is_controlled = !$Person2.is_controlled
+	clamp($Person1.position.x, 0, screensize.x)
+	clamp($Person2.position.x, 0, screensize.x)
+	clamp($Person1.position.y, 0, screensize.y)
+	clamp($Person2.position.y, 0, screensize.y)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _on_walkbutton_toggled(buttonbool):
+	$Person1.is_controlled = buttonbool
+	$Person2.is_controlled = !buttonbool
+
+func _on_returnbutton_pressed():
+	$Person1.position = Vector2(300,300)
+	$Person2.position = Vector2(600,300)
--- Person.gd
+++ Person.gd
@@ -5,29 +5,56 @@ extends RigidBody2D
 # var a = 2
 # var b = "text"
 
-export var speed = 200
+var is_controlled = true
+var direction = Vector2(1,0)
+export var speed = 100
+var velocity = Vector2(0,0)
+export var rotation_rate = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	velocity = Vector2(1,0) * speed
 
-func _physics_process(delta):
-	move()
+func _process(delta):
+	if is_controlled:
+		$DirectionalArrow.show()
+		rotate_arrow()
+		rotate_velocity(delta)
+		direction = velocity.normalized()
+	else:
+#		velocity = Vector2(0,0)
+		$DirectionalArrow.hide()
+	move(delta)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-func move():
-	linear_velocity = Vector2(0,0)
-	if Input.is_action_pressed("keyboard_down"):
-		linear_velocity += Vector2(0,1)
+func rotate_velocity(delta):
 	if Input.is_action_pressed("keyboard_left"):
-		linear_velocity += Vector2(-1,0)
+		velocity= velocity.rotated(-rotation_rate*delta)
 	if Input.is_action_pressed("keyboard_right"):
-		linear_velocity += Vector2(1,0)
-	if Input.is_action_pressed("keyboard_up"):
-		linear_velocity += Vector2(0,-1)
-	if linear_velocity != Vector2(0,0):
-		linear_velocity = linear_velocity.normalized() * speed
-		
+		velocity= velocity.rotated(rotation_rate*delta)
+
+func move(delta):
+#	direction = velocity.normalized()
+	position += velocity * delta
+
+#func move():
+#	var direction = Vector2(0,0)
+##	if Input.is_action_pressed("keyboard_down"):
+##		direction += Vector2(0,1)
+#	if Input.is_action_pressed("keyboard_left"):
+#		direction += Vector2(-1,0)
+#	if Input.is_action_pressed("keyboard_right"):
+#		direction += Vector2(1,0)
+##	if Input.is_action_pressed("keyboard_up"):
+##		direction += Vector2(0,-1)
+##	if direction != Vector2(0,0):
+##		linear_velocity = direction.normalized() * speed
+##		is_walking = true
+##	else:
+##		is_walking = false
+
+func rotate_arrow():
+	$DirectionalArrow.rotation = velocity.angle()
