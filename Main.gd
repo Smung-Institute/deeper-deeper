24e26259f589a607bb34a80f9ba3c311f3bc16fe
ducksoftheworld <ducksoftheworld@users.noreply.github.com>
2021-04-24 12:25:06 -0400

    splitscreen

--- Main.gd
+++ Main.gd
@@ -1,14 +1,20 @@
 extends Node
 
-var screensize = Vector2(600,400)
-onready var world = $GameWorld
+var screensize = Vector2(300,400)
+onready var world = $MarginContainer/HBoxContainer2/Viewports/ViewportContainerL/Viewport/GameWorld
 onready var worldmap = world.get_node("Map")
-onready var playerl = $GameWorld/Map/Person1
-onready var playerr = $GameWorld/Map/Person2
+onready var playerl = world.get_node("Map/Person1")
+onready var playerr = world.get_node("Map/Person2")
 
+onready var viewportr = get_node("MarginContainer/HBoxContainer2/Viewports/ViewportContainerR/Viewport")
+onready var viewportl = get_node("MarginContainer/HBoxContainer2/Viewports/ViewportContainerL/Viewport")
 
+onready var camerar = viewportr.get_node("Camera2DR")
+onready var cameral = viewportl.get_node("Camera2DL")
 
 func _ready():
+	world = viewportl.find_world_2d()
+	viewportr.world_2d = world
 	playerl.is_controlled = false
 	playerr.is_controlled = true
 	
@@ -16,10 +22,13 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		playerl.is_controlled = !playerl.is_controlled
 		playerr.is_controlled = !playerr.is_controlled
-	clamp(playerl.position.x, 0, screensize.x)
-	clamp(playerr.position.x, 0, screensize.x)
-	clamp(playerl.position.y, 0, screensize.y)
-	clamp(playerr.position.y, 0, screensize.y)
+#	playerl.position.x = clamp(playerl.position.x, 0, screensize.x)
+#	playerr.position.x = clamp(playerr.position.x, 0, screensize.x)
+#	playerl.position.y = clamp(playerl.position.y, 0, screensize.y)
+	cameral.position = playerl.position
+	camerar.position = playerr.position
+	
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
