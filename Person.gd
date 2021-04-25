e258078694c8c7a8e981a6b68a24730bbbfe8f29
Ben <ishmandoo@gmail.com>
2021-04-24 22:31:19 -0400

    improved title screen

--- Person.gd
+++ Person.gd
@@ -2,6 +2,11 @@ extends RigidBody2D
 
 var torch_scene = preload("res://Torch.tscn")
 
+var return_position
+
+var TORCH_COOLDOWN = 10
+var last_torch_time = TORCH_COOLDOWN
+
 var is_controlled = true
 var direction = Vector2(1,0)
 export var speed = 100
@@ -14,6 +19,7 @@ func _ready():
 	linear_velocity = Vector2(1,0) * speed
 	connect("body_entered", self, "_handle_body_entered")
 	$AnimatedSprite.animation = name
+	return_position = position
 	
 func _handle_body_entered(body):
 	if body.is_in_group("bat"):
@@ -24,14 +30,19 @@ func _handle_body_entered(body):
 		die()
 		
 func die():
-	print("I died")
+	position = return_position
+
 
 func _process(delta):
+	last_torch_time += delta
 	if Input.is_action_just_pressed("keyboard_down"):
 		if is_controlled:
-			var torch = torch_scene.instance()
-			torch.position = position
-			get_parent().add_child(torch)	
+			if last_torch_time > TORCH_COOLDOWN:
+				var torch = torch_scene.instance()
+				torch.position = position
+				get_parent().add_child(torch)
+				return_position = position
+				last_torch_time = 0
 
 func _physics_process(delta):
 	linear_velocity = linear_velocity.normalized() * speed
--- TitleScreen.gd
+++ TitleScreen.gd
@@ -6,10 +6,15 @@ extends Node
 # var b = "text"
 
 onready var startbutton = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/Panel/StartButton
+onready var storybutton = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/Panel4/StoryButton
+onready var creditsbutton = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/Panel5/CreditsButton
 var fliprate = 0.1
 onready var topl = $MarginContainer/VBoxContainer/HBoxContainer/Panel2/AnimatedSprite
 onready var topr = $MarginContainer/VBoxContainer/HBoxContainer/Panel/AnimatedSprite2
 onready var tween = $Fader/Tween
+
+onready var story_audio = $Story
+onready var credits_audio = $Credits
 
 onready var game_scene_path = "res://Main.tscn"
 
@@ -27,9 +32,18 @@ func _ready():
 	tween.start()
 	tween.connect("tween_all_completed", self, "_on_fade_finished")
 	startbutton.connect("pressed", self, "_on_startbutton_pressed")
+	storybutton.connect("pressed", self, "_on_storybotton_pressed")
+	creditsbutton.connect("pressed", self, "_on_creditsbotton_pressed")
 	
 func _on_fade_finished():
 	$Fader.hide()
+	
+func _on_storybotton_pressed():
+	story_audio.play()
+	
+func _on_creditsbotton_pressed():
+	credits_audio.play()
+	
 	
 func _on_startbutton_pressed():
 	$Fader.show()
